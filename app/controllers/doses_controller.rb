cc594f788e73615401f6f049a28bf5772a742ad3
Diosa Taylor <diosataylor@yahoo.com>
2017-03-24 11:43:19 +0100

    back end finished

--- app/controllers/doses_controller.rb
+++ app/controllers/doses_controller.rb
@@ -2,30 +2,30 @@ class DosesController < ApplicationController
 
   def new
     @cocktail = Cocktail.find(params[:cocktail_id])
-    @ingredient = Ingredient.find(params[:ingredient_id])
+    # @ingredient = Ingredient.find(params[:ingredient_id])
     @dose = Dose.new
   end
 
   def create
     @cocktail = Cocktail.find(params[:cocktail_id])
-    @ingredient = Ingredient.find(params[:ingredient_id])
+    # @ingredient = Ingredient.find(params[:ingredient_id])
 
     @dose = Dose.new(dose_params)
     @dose.cocktail = @cocktail
-    @dose.ingredient = @ingredient
+    # @dose.ingredient = @ingredient
 
     if @dose.save
       redirect_to cocktail_path(@dose.cocktail)
     else
-      render :new
+      @doses = @cocktail.doses
+      render "cocktails/show"
     end
   end
 
   def destroy
-    @cocktail = @dose.cocktail
-
+    @dose = Dose.find(params[:id])
     @dose.destroy
-    redirect_to cocktail_path(@cocktail)
+    redirect_to cocktail_path(@dose.cocktail)
   end
 
   private
